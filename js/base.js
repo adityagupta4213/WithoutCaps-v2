ce90fa72de77a6e19b3221ff6b2b475c2371949a
Aditya J Gupta <adityagupta4213@yandex.com>
2017-04-09 00:06:25 +0530

    Changed backgrounds and nav-bar. Added new product

--- js/base.js
+++ js/base.js
@@ -6,28 +6,11 @@ var is_menu_open = false;
 
 $('.menu > .icon').on('click', function() {
     if (!(is_menu_open)) {
-        $('.menu').css({'background': 'white','color': '#111010'});
         $('.expanded-menu').css('display', 'block');
         is_menu_open = true;
     } else {
-        $('.menu').css({
-            'background': '#111010',
-            'color': 'white'
-        });
         $('.expanded-menu').css('display', 'none');
         is_menu_open = false;
-    }
-});
-
-$(window).scroll(function() {
-    if ($(this).scrollTop() > 500) {
-        $('.menu').css('display', 'none');
-        $('.expanded-menu').fadeIn(200);
-        $('.expanded-menu').css('box-shadow', '0 0 5px gray');
-    } else {
-        $('.expanded-menu').css('display', 'none');
-        $('.expanded-menu').css('box-shadow', 'none');
-        $('.menu').css('display', 'block');
     }
 });
 
